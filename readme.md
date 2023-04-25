09dc8d50c2b2ede4134cc7c26913868eb8ef51d3
Mariana Spaccesi <marianaism.smi@gmail.com>
2023-04-25 18:15:56 -0300

    app con node js

---
+++ readme.md
@@ -0,0 +1,13 @@
+# Modulos de Producción:
+
+# inicializo un package.json con el comando npm init -y
+# instalo un framew como express para crear un servidor : npm i express 
+# instalo morgan para ver por consola las request que llegan del nav: npm i morgan 
+# instalo ejs para generar páginas html: npm i ejs. Extiende codigo de js al html.  
+
+# Modulos de Desarrollo (devDependencies):
+# nodemon: npm i nodemon -D (el flag -D indica que es de desarrollo) - ejecutar: npx ndemon filePath , o hacer script en el package.json npm run scriptKey
+
+# Configurar Express 
+# en el pckge añadir "type": "module" para que al ejecutar el codigo se empleen automaticamente los imports y exports (import from - como en react) 
+
---
+++ src/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import {dirname, join} from 'path';
+import { fileURLToPath } from "url";
+import indexRoutes from './routes/index.js'
+
+const app = express();
+//creo una app indicando que es la ejecución de express. 
+
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+//el modulo de dirname permite convertir rutas abs a dynamics
+//C:\Users\maari\NodeJS\Proyecto_Nodejs\src
+
+app.set("views", join(__dirname, "views"))
+//el modulo de join permite unificar criterios de path de linux y wdw
+
+app.set('view engine', 'ejs')
+//agrego config para extender el html  
+
+app.use(indexRoutes)
+//indico que importe el export por default de ./routes/index.js
+
+app.use(express.static(join(__dirname, 'public')))
+//traigo la ruta dynam de mi proy y le agrego la carpeta de public 
+
+
+
+app.listen(3000)
+// le indico que escuche en el puerto 3000 
+
